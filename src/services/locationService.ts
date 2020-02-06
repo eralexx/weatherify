19f6f06993e4d01ebc7cbb84cfa25b8bc8830fd0
Alex Gonzalez Jucla <agonzalez@softeng.es>
2020-02-06 18:35:15 +0100

    location service

--- src/services/locationService.ts
+++ src/services/locationService.ts
@@ -1,1 +1,15 @@
-export default class LocationService {}
+import ICoordinates from "../entities/ICoordinates";
+
+export default class LocationService {
+  public static getBrowserLocation = (): Promise<ICoordinates> => {
+    return new Promise<ICoordinates>((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          const longitude = position.coords.longitude;
+          const latitude = position.coords.latitude;
+          resolve({ latitude, longitude, available: true });
+        });
+      } else reject({ available: false, latitude: 0, longitude: 0 });
+    });
+  };
+}
