abbe384bfd35db13a053850345b4f38d710dd872
Alex Gonzalez Jucla <agonzalez@softeng.es>
2020-02-07 13:40:25 +0100

    minor changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Row, Col, InputGroup, FormControl } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 class Header extends React.Component<any, any> {
   public render() {
@@ -19,20 +19,7 @@ class Header extends React.Component<any, any> {
           <Col sm={8}>
             <h3>Or browse other place's forecast...</h3>
           </Col>
-          <Col sm={4}>
-            <InputGroup className="mb-3">
-              <FormControl
-                placeholder="Recipient's username"
-                aria-label="Recipient's username"
-                aria-describedby="basic-addon2"
-              />
-              <InputGroup.Append>
-                <InputGroup.Text id="basic-addon2">
-                  @example.com
-                </InputGroup.Text>
-              </InputGroup.Append>
-            </InputGroup>
-          </Col>
+          <Col sm={4}></Col>
         </Row>
       </>
     );
