e32ca9a1126dbbb4036d027bdb232a10422fa6f2
Alex Gonzalez Jucla <agonzalez@softeng.es>
2020-02-21 10:26:23 +0100

    casual changes

--- src/components/ToggleUnits/ToggleUnits.tsx
+++ src/components/ToggleUnits/ToggleUnits.tsx
@@ -3,28 +3,25 @@ import * as React from "react";
 import Switch from "@material-ui/core/Switch";
 import IGlobalState from "../../globalState/IGlobalState";
 import Context from "../../globalState/Context";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import UnitsList from "../../entities/Units";
 
 const ToggleUnits = () => {
-  const {
-    Loaded,
-    Forecast,
-    Units,
-    ToggleUnits
-  }: IGlobalState = React.useContext(Context);
+  const { Units, ToggleUnits }: IGlobalState = React.useContext(Context);
   return (
-    <Row>
-      <Col>Off</Col>
-      <Col>
-        <Switch
-          checked={Units === UnitsList.Metric ? true : false}
-          onChange={ToggleUnits}
-          value={Units}
-        />
-      </Col>
-      <Col>On</Col>
-    </Row>
+    <Container>
+      <Row>
+        <Col sm={3}>Imperial</Col>
+        <Col sm={3}>
+          <Switch
+            checked={Units === UnitsList.Metric ? true : false}
+            onChange={ToggleUnits}
+            value={Units}
+          />
+        </Col>
+        <Col sm={3}>Metric</Col>
+      </Row>
+    </Container>
   );
 };
 
